2d5b38db1e69d0b8580069a8d3eefbee91999e1f
anandvipul <anandvipul01@gmail.com>
2023-02-09 18:12:11 +0530

    class component

---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+    render () {
+        return <h4>Hello, I am A Welcome Component from Class Component</h4>;
+    }
+}
+
+
+export default Welcome;
